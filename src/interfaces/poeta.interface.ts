b0a7fc2e1041b641cfc410ee5a61a07f844fa132
Carlos Ernesto Millán <encuadrante@gmail.com>
2018-07-07 19:55:52 -0500

    Primer commit

---
+++ src/interfaces/poeta.interface.ts
@@ -0,0 +1,7 @@
+export interface Poeta {
+  nombre: string;
+  imagen: string;
+  audio: string;
+  duracion: number;
+  reproduciendo: boolean;
+}
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,37 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { POETAS } from "../../data/data.poetas";
+import { Poeta } from "../../interfaces/poeta.interface";
+
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+    poetas:Poeta[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+    constructor(){
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+      this.poetas = POETAS.splice(0);
 
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
     }
-  }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
-  }
 }
