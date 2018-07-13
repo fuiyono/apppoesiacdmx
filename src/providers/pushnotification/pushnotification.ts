5c1bfa8a1b7bcb8837d91e7789d4b486eb45909c
Carlos Ernesto Millán <encuadrante@gmail.com>
2018-07-12 21:52:35 -0500

    imagenes

--- src/providers/pushnotification/pushnotification.ts
+++ src/providers/pushnotification/pushnotification.ts
@@ -1,33 +1,35 @@
-
 import { Injectable } from '@angular/core';
 import { OneSignal } from '@ionic-native/onesignal';
-/*
-  Generated class for the PushnotificationProvider provider.
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+import { Platform } from 'ionic-angular';
+
 @Injectable()
 export class PushnotificationProvider {
 
-  constructor( private oneSignal: OneSignal) {
+  constructor(private oneSignal: OneSignal,
+              public platform: Platform) {
     console.log('Hello PushnotificationProvider Provider');
   }
 
   init_notifications(){
-    this.oneSignal.startInit('ba3b0195-e142-4cb4-b04d-757c4b0f3a56', '672933041681');
 
-this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+      this.oneSignal.startInit('ba3b0195-e142-4cb4-b04d-757c4b0f3a56', '672933041681');
 
-this.oneSignal.handleNotificationReceived().subscribe(() => {
- // do something when notification is received
-});
+      this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
 
-this.oneSignal.handleNotificationOpened().subscribe(() => {
-  // do something when a notification is opened
-});
+      this.oneSignal.handleNotificationReceived().subscribe(() => {
+       // do something when notification is received
+       console.log('Notificacion recibida');
+      });
 
-this.oneSignal.endInit();
+      this.oneSignal.handleNotificationOpened().subscribe(() => {
+        // do something when a notification is opened
+        console.log('Notificacion abierta');
+      });
+
+      this.oneSignal.endInit();
+
+
   }
 
 }
